b8ec49dc00953572da863d00f980b1c530a3738b
Joseph Chow <joefstack@gmail.com>
2022-04-01 11:51:52 -0400

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+import Attributions from "./Attributions/Attributions";
+
+export default function Main() {
+  return (
+    <Fragment>
+      <div className="container"></div>
+      <Attributions />
+    </Fragment>
+  );
+}
